022cf8a9b1b4e82752e5be1a2bcf765cbf1a86b5
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-06-29 17:13:32 +0530

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 flag = 0
 for i in range(122, 96, -1):
-    print(f'{chr(i - flag)}', end='')
+    print('{}'.format(chr(i - flag)), end='')
     if (flag == 0):
         flag = 32
     else:
