5a9eb2b3fce1b3d8c997a04dfc689e018062dabc
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-29 04:16:50 -0700

    edit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_num = str(number)[-1]
+print("Last digit of", number, "is", last_num, end=" ")
+if (int(last_num) > 5):
+    print("and is greater than 5")
+elif (int(last_num) == 0):
+    print("and is 0")
+elif (int(last_num) < 6):
+    print("and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    print(repr(number)[-1], end='')
+    return (repr(number)[-1])
