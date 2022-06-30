0dae42d07c65add9f4cdb17925f4b72538c8db8a
mazi-kunle <olakunlemichael644@gmail.com>
2022-06-30 03:51:17 -0700

    edit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * check_add- a function that checks if an address is in an array.
- * @ptr: address to check.
- * @arr: array to check if address is present.
- *
- * Return: 0 if address not present and 1 if present.
-*/
-int check_add(listint_t *ptr, listint_t **arr)
-{
-	int length = 100;
-	int i;
-
-	for (i = 0; i < length; i++)
-	{
-		if (arr[i] == ptr)
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-/**
  * check_cycle- a function that checks if a singly linked list has
  * a cycle in it.
  * @list: list to check.
@@ -30,35 +9,18 @@ int check_add(listint_t *ptr, listint_t **arr)
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp;
-	listint_t **arr = malloc(100 * sizeof(listint_t *));
-	int i = 0;
-	int flag;
+	listint_t *fast, *slow;
 
-	if (list == NULL)
+	fast = list;
+	slow = list;
+	while (fast && slow && fast->next)
 	{
-		return (0);
-	}
-	temp = list;
-	while (temp != NULL)
-	{
-		if (check_add(temp, arr) == 0)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-			arr[i] = temp;
-			i++;
-			flag = 1;
+			return (1);
 		}
-		else
-		{
-			flag = 0;
-			break;
-		}
-		temp = temp->next;
 	}
-	free(arr);
-	if (flag == 1)
-	{
-		return (0);
-	}
-	return (1);
+	return (0);
 }
