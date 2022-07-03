63ec60617259e4a78a43af24ed6db9e173678971
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-03 20:20:57 +0530

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    sum = 0
     if len(my_list) == 0:
         return None
+    sum = my_list[0]
     for i in my_list:
         if i > sum:
             sum = i
