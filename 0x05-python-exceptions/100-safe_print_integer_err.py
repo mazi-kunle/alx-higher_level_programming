9a7a0d21dcc6fce8492faf8ee53c9bf9d1e76d49
mazi-kunle <olakunlemichael644@gmail.com>
2022-07-19 08:05:33 -0700

    edit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    import sys
+
+    try:
+        print("{:d}".format(value))
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    else:
+        return True
