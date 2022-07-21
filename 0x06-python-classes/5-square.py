2fdccf98e7f7b97dcefbe4c5db365382d69b8260
mazi-kunle <olakunlemichael644@gmail.com>
2022-07-21 03:54:46 -0700

    edit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -26,3 +26,13 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        """A method to print a square."""
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
