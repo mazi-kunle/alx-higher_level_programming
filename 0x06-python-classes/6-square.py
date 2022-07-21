2b1ec1fa452dbd06c8aeefd29d3ce9cf8b594ecd
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-21 05:13:11 -0700

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,7 +52,7 @@ class Square:
         if (self.__size == 0):
             print()
         else:
-            print(' ' * self.__position[1], end='')
+            [print("") for i in range(0, self.__position[1])]
             for i in range(self.__size):
                 print(' ' * self.__position[0], end='')
                 for j in range(self.__size):
