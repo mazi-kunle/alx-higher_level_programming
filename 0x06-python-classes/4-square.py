13ad6f335bdb5b63a71f1e622b87449f5944f6b5
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-21 03:25:58 -0700

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,14 +2,25 @@
 class Square:
     """A Square class that defines a square"""
     def __init__(self, size=0):
+        """init function."""
+        self.size = size
+
+    def area(self):
+        """A method to get the area of a sqaure"""
+        return (self.__size ** 2)
+    
+    @property
+    def size(self):
+        """A getter function."""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """A setter function."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
-    def area(self):
-        """A method to get the area of a sqaure"""
-        return (self.__size ** 2)
     
