43c4686a8ad8bd748967c1a135bf8e6199e1d07e
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-24 16:50:02 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,12 +12,9 @@ def add_integer(a, b=98):
 
     Returns an integer: the addition of a and b.
     '''
-    if (type(a) != int or type(b) != int or
-            type(a) != float or type(b) != float):
-        raise TypeError('a must be an integer or b must be an integer')
-    elif type(a) == float:
-        a = int(a)
-    elif type(b) == float:
-        b = int(b)
+    if (type(a) != int and type(a) != float):
+        raise TypeError('a must be an integer')
+    elif (type(b) != int and type(b) != float):
+        raise TypeError('b must be an integer')
     else:
-        return a + b
+        return int(a) + int(b)
