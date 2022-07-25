55018e477999f79c3601d82be9c380c8cb193ef6
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-25 12:17:49 -0700

    Create 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+
+def text_indentation(text):
+   '''
+   a function that prints a text with 2 new lines after
+   each of these characters: ., ? and :
+   '''
+   c = 0
+   while c < len(text) and text[c] == ' ':
+      c += 1
+   while c < len(text):
+      print(text[c], end='')
+      if text[c] in '.?:':
+         print("\n")
+         c += 2
+         continue
+      else:
+         c += 1
