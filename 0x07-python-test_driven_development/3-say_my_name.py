9df33432a1c6d08f58967de1e3cfde2480520ddb
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-31 19:10:47 -0700

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,8 +6,8 @@ def say_my_name(first_name, last_name=""):
     '''
     a function that prints My name is <first name> <last name>
     '''
-    if (type(firstname) != str):
+    if (type(first_name) != str):
         raise TypeError('first_name must be a string')
-    elif (type(lastname) != str):
+    elif (type(last_name) != str):
         raise TypeError('last_name must be a string')
     print("My name is {} {}".format(first_name, last_name))
