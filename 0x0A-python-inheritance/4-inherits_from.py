dee7b766b394960fa5ee71cf2a294c4fa29151d9
mazi-kunle <olakunlemichael644@gmail.com>
2022-08-01 14:46:53 -0700

    2

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    A function that returns True if the object is exactly an instance
+    of the specified class.
+    '''
+    return (isinstance(obj, a_class))
