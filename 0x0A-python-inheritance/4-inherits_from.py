2749d79a130567b6c419c2c060a052a3b39ca8ef
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-08-01 14:50:20 -0700

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ def inherits_from(obj, a_class):
     A function that returns True if the object is exactly an instance
     of the specified class.
     '''
-    return (isinstance(obj, a_class))
+    return (isinstance(obj, a_class) and type(obj) != a_class)
