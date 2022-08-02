bf06a5730c84d9c61362fdfdcc457a98d77e910b
mazi-kunle <olakunlemichael644@gmail.com>
2022-08-02 09:12:18 -0700

    edit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+
+class BaseGeometry:
+    '''This is a class'''
+    def area(self):
+        '''An area class'''
+        rasie Exception('area() is not implemented')
