341f1372285166385e12719af310f25a28184bd4
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-08-03 16:14:52 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,4 +8,4 @@ def append_write(filename="", text=""):
     a text file (UTF8) and returns the number of characters added:
     '''
     with open(filename, 'a', encoding='Utf-8') as f:
-        f.write(text)
+        Return f.write(text)
