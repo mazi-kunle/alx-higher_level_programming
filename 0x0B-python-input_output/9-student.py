e2b7ab607c704687191fd2a148670f23675c9e4b
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-08-04 16:35:37 +0100

    Create 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+
+class Student:
+    '''A student class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+        A function that returns an instance attribute.
+        '''
+        Return self.__dict__
