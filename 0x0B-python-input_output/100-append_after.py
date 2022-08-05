be039a672344ff9c4748dfcb1087072c9d7a412b
mazi-kunle <olakunlemichael644@gmail.com>
2022-08-05 12:41:35 -0700

    edit

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,13 +9,13 @@ def append_after(filename="", search_string="", new_string=""):
     '''
     with open(filename, 'r') as f:
         a = f.readlines()
-    
+
     line = 0
     for i in a[:]:
         if search_string in i:
             line += 1
             a.insert(line, new_string)
         line += 1
-        
+
     with open(filename, 'w') as f:
         f.writelines(a)
