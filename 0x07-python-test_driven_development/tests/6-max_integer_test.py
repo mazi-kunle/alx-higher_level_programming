258520cffc3e441e649cc72bdf41500930e09d81
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-08-06 13:19:28 -0700

    Create 6-max_integer_test.py

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+import unittest
+
+class test_max_integer(unittest.TestCase):
+    def test_max(self):
+        a = [0,1,2,3]
+        self.assertAlmostEqual(max_integer(a), 3)
+
+    def test_max2(self):
+        a = [-1,-2,-3]
+        self.assertAlmostEqual(max_integer(a), -1)
+
+    def test_max3(self):
+        a = []
+        self.assertAlmostEqual(max_integer(a), None)
+
+    def test_max5(self):
+        a = [3,2,1]
+        self.assertAlmostEqual(max_integer(a), 3)
+
+    def test_max6(self):
+        a = [2]
+        self.assertAlmostEqual(max_integer(a), 2)
+
+    def test_max4(self):
+        a = ['a','b',1]
+        self.assertRaises(TypeError, max_integer, a)
