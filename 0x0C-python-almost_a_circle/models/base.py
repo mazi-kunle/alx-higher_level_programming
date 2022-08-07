c13aa36f7b776ab7881c6035487de51324ca65e6
mazi-kunle <olakunlemichae644@gmail.com>
2022-08-07 12:16:03 -0700

    new

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,16 @@ class Base:
         '''
         return the list of the json string representation.
         '''
-        if json_String is None or len(json_String) == 0:
+        if json_string is None or len(json_string) == 0:
             return []
 
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''
+        returns an instance with all attributes already set:
+        '''
+        dummy_instance = cls(1, 2)
+        cls.update(dictionary)
+        return dummy_instance
