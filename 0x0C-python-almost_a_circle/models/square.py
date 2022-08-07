ecea6801ed9ae493797c39af50b4f23ca4e82666
mazi-kunle <olakunlemichae644@gmail.com>
2022-08-07 04:18:25 -0700

    11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,18 @@ class Square(Rectangle):
         '''A setter function for size'''
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        '''Update method for square'''
+        if len(args) != 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+
+            except IndexError:
+                pass
+        else:
+            for key, val in kwargs.items():
+                setattr(self, key, val)
