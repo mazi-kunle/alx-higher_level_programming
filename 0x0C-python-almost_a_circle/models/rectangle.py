d6275c448c2b3856f00aae7c7e1a9629f23e8035
mazi-kunle <olakunlemichae644@gmail.com>
2022-08-07 02:11:29 -0700

    finish task 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,9 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        '''
+        A method that returns the area value of the Rectangle instance.
+        '''
+        return self.__width * self.__height
