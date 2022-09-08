921628fefa3eb521d96317e4ecb0b4582156091b
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-09-07 17:59:30 -0700

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,6 +8,3 @@ ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- table name
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- field name
-ALTER TABLE `first_table` CHANGE name name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
