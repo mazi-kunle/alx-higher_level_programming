a2dedc1cba86c7bb338d8803fb5736f817371437
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-09-09 16:48:13 -0700

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@ USE hbtn_0d_tvshows;
 SELECT tv_shows.title as title, tv_show_genres.genre_id as genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv.shows.id = tv_show_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
