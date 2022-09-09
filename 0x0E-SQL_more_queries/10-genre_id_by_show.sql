7e74795bbd02676cc957016d6e837614fd370855
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-09-09 16:54:42 -0700

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- list all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-USE hbtn_0d_tvshows;
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
