c139b81255e91af3ea28a10767b1244f0f03ae49
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-09-09 17:19:14 -0700

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,6 +5,6 @@
 
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-FULL OUTER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
