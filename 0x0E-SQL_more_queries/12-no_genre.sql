83dcbb401b27b681fdf72240adeb88160b72e9e3
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-09-09 17:46:06 -0700

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -6,5 +6,5 @@ SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 FULL OUTER JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_shows.id=NULL OR WHERE tv_show_genres.show_id=NULL
+WHERE tv_shows.id=NULL OR WHERE tv_show_genres.genre_id=NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
