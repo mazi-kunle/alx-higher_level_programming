dcb29d19364ceee11721c70af01c16a494956e8b
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-09-09 17:10:54 -0700

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,7 +4,7 @@
 -- If a show doesn’t have a genre, NULL is displayed
 
 SELECT tv_shows.title AS title, tv_show_genres.genre_id as genre_id
-FROM tv.shows
+FROM tv_shows
 FULL OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
