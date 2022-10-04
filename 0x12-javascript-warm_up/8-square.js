9a729b2c5394fad3c2736cf9c0f978a732bb13cd
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-04 07:27:04 -0700

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let num = process.argv[2];
+
+if (!num || !parseInt(num)) {
+  console.log('Missing size');
+} else {
+  num = parseInt(num);
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
