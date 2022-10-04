ac9b0fc9f1fd092fefcb832dbafb307aa5158a73
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-04 06:01:16 -0700

    4

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,6 @@ const argv = process.argv;
 
 if (!argv[2]) {
   console.log('No argument');
-} else if (argv[3]) {
-  console.log('Arguments found');
 } else {
-  console.log('Argument found');
+  console.log(argv[2]);
 }
