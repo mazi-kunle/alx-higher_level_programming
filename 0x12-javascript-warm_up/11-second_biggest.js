848e08a4cc26fac8d626e290a3001e5f7fa7feda
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-05 04:40:09 -0700

    11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(1);
+} else {
+  let first = -Infinity;
+  let second = -Infinity;
+  const arr = process.argv;
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] > second && arr[i] !== first) {
+      second = arr[i];
+    }
+  }
+  console.log(second);
+}
