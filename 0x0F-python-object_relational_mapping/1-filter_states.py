54c51744d1f4cc5193ea2a89bc9e6a1e9936e52c
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-07 16:54:05 -0700

    2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect('localhost', sys.argv[1],
+                         sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%"')
+    for i in cursor.fetchall():
+        print(i)
