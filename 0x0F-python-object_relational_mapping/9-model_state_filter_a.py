54377669727608da033c2551abfd37e459db9bb9
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-09 02:58:48 -0700

    9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''This is a module'''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            username,
+                            password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%'))
+    for state in states:
+        print(f'{state.id}: {state.name}')
