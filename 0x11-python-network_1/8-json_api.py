3a59cabe43e2c1af1f39a8cf64317e185e493ec3
mazi-kunle <olakunlemichael644@gmail.com>
2022-10-27 07:16:59 -0700

    6

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+'''
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    data = {'q': None}
+    if len(argv) == 1:
+        data['q'] = ''
+    else:
+        data['q'] = argv[1]
+
+    r = requests.post('http://0.0.0.0:5000/search_user',
+                      data=data)
+    try:
+        json_data = r.json()
+    except requests.exceptions.JSONDecodeError:
+        print('Not a  valid JSON')
+    else:
+        if (len(json_data) == 0):
+            print('No result')
+        else:
+            print('[{}] {}'.format(json_data['id'],
+                  json_data['name']))
