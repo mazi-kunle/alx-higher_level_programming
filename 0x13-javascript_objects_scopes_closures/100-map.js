4d2e0960795dc0d2136992788bc248ad91912453
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-12-23 03:27:36 -0800

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const arr = require('./100-data.js').list;
-const arr2 = arr.map(function (element, index) { return (index * element); });
-console.log(arr);
-console.log(arr2);
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  const nlist = [];
-  while (list.length > 0) {
-    nlist.push(list.pop());
-  }
-  return (nlist);
-};
